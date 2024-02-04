ff8f8014b850b3aea39c906464b6d2aa093c5bba
MAGGIOLI\alex.lonzi <alex.lonzi@maggioli.it>
2024-02-04 18:38:54 +0100

    feat(dettaglio nave): setup della lista dei giorni con dipendenti assegnati alla nave

--- src/Template.Web/Areas/Example/Navi/EditViewModel.cs
+++ src/Template.Web/Areas/Example/Navi/EditViewModel.cs
@@ -1,27 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Template.Services.Shared;
 using Template.Web.Infrastructure;
+using static Template.Services.Shared.ArriviDTO;
 
 namespace Template.Web.Areas.Example.Navi
 {
     [TypeScriptModule("Example.Navi.Server")]
     public class EditViewModel
     {
-        public EditViewModel()
+        public EditViewModel() 
         {
+            DateLavorazione = Array.Empty<DateLavorazioneEditViewModel>();
         }
 
         public Guid? Id { get; set; }
-        [Display(Name = "Nome")]
         public string Nome { get; set; }
-        [Display(Name = "Cognome")]
         public string NomeCliente { get; set; }
         public DateTime Arrivo { get; set; }
         public DateTime Partenza { get; set; }
         public int Container {  get; set; }
         public int Bancali { get; set; }
 
+        public IEnumerable<DateLavorazioneEditViewModel> DateLavorazione { get; set; }
+
+        internal void SetDateLavorazione(OrariNaveSelectDTO orariNaveSelectDTO)
+        {
+            DateLavorazione = orariNaveSelectDTO.Orari.Select(x => new DateLavorazioneEditViewModel(x)).ToArray();
+        }
 
         public string ToJson()
         {
@@ -41,5 +49,15 @@ namespace Template.Web.Areas.Example.Navi
                 Bancali = naveDetailDTO.Bancali;
             }
         }
+    }
+
+    public class DateLavorazioneEditViewModel
+    {
+        public DateLavorazioneEditViewModel(OrariNaveSelectDTO.Orario dateLavorazioneDTO)
+        {
+            this.Data = dateLavorazioneDTO.Giorno;
+        }
+
+        public DateOnly Data { get; set; }
     }
 }
